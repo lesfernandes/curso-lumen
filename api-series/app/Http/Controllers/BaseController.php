4f8c0804edd6443c29bbd357658af507c94f4453
Letícia Fernandes <lemayara16@gmail.com>
2020-12-22 15:34:41 -0300

    controller abstrato

---
+++ api-series/app/Http/Controllers/BaseController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+abstract class BaseController
+{
+    protected $classe;
+
+    public function index()
+    {
+        return $this->classe::all();
+    }
+
+    public function store(Request $request)
+    {
+        return response()
+                ->json(
+                    $this->classe::create($request->all()),
+                    201
+                );
+    }
+
+    public function show(int $id)
+    {
+        $recurso = $this->classe::find($id);
+
+        if(is_null($recurso)) {
+            return response()->json('', 204);
+        }
+
+        return response()->json($recurso);
+    }
+
+    public function update(int $id, Request $request)
+    {
+        $recurso = $this->classe::find($id);
+        if(is_null($recurso)) {
+            return response()->json(['erro' => 'Recurso não encontrado.'], 404);
+        }
+        // função 'fill' preenche o objeto de acordo com os parâmetros
+        $recurso->fill($request->all());
+        $recurso->save();
+    }
+
+    public function destroy(int $id)
+    {
+        $quantidadeRecursosRemovidos = $this->classe::destroy($id);
+        if ($quantidadeRecursosRemovidos === 0){
+            return response()->json([
+                'erro' => 'Recurso não encontrado.'
+            ], 404);
+        }
+        return response()->json('', 204);
+    }
+}
--- api-series/app/Http/Controllers/SeriesController.php
+++ api-series/app/Http/Controllers/SeriesController.php
@@ -3,54 +3,11 @@
 namespace App\Http\Controllers;
 
 use App\Models\Serie;
-use Illuminate\Http\Request;
 
-class SeriesController
+class SeriesController extends BaseController
 {
-    public function index()
+    public function __construct()
     {
-        return Serie::all();
-    }
-
-    public function store(Request $request)
-    {
-        return response()
-                ->json(
-                    Serie::create(['nome' => $request->nome]),
-                    201
-                );
-    }
-
-    public function show(int $id)
-    {
-        $serie = Serie::find($id);
-
-        if(is_null($serie)) {
-            return response()->json('', 204);
-        }
-
-        return response()->json($serie);
-    }
-
-    public function update(int $id, Request $request)
-    {
-        $serie = Serie::find($id);
-        if(is_null($serie)) {
-            return response()->json(['erro' => 'Recurso não encontrado.'], 404);
-        }
-        // função 'fill' preenche o objeto de acordo com os parâmetros
-        $serie->fill($request->all());
-        $serie->save();
-    }
-
-    public function destroy(int $id)
-    {
-        $quantidadeRecursosRemovidos = Serie::destroy($id);
-        if ($quantidadeRecursosRemovidos === 0){
-            return response()->json([
-                'erro' => 'Recurso não encontrado.'
-            ], 404);
-        }
-        return response()->json('', 204);
+        $this->classe = Serie::class;
     }
 }

65fbbd5e32402b4d4d7c1ec487714380898d1568
Letícia Fernandes <lemayara16@gmail.com>
2020-12-22 11:59:02 -0300

    criação do controller de series e da rota

---
+++ api-series/app/Http/Controllers/SeriesController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class SeriesController
+{
+    public function index() 
+    {
+
+        return [
+            "Grey's Anatomy", 
+            "Lost"
+        ];
+
+    }
+}
--- api-series/routes/web.php
+++ api-series/routes/web.php
@@ -13,6 +13,11 @@
 |
 */
 
+/** @var \Laravel\Lumen\Routing\Router $router */
 $router->get('/', function () use ($router) {
     return $router->app->version();
+});
+
+$router->group(['prefix' => '/api'], function() use ($router) {
+    $router->get('/series', 'SeriesController@index');
 });

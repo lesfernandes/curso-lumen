7025d76d9bcd619d5a2f158abf001d5f7d9d6cd8
Letícia Fernandes <lemayara16@gmail.com>
2020-12-22 14:46:02 -0300

    post e get de serie

--- api-series/app/Http/Controllers/SeriesController.php
+++ api-series/app/Http/Controllers/SeriesController.php
@@ -3,11 +3,32 @@
 namespace App\Http\Controllers;
 
 use App\Models\Serie;
+use Illuminate\Http\Request;
 
 class SeriesController
 {
-    public function index() 
+    public function index()
     {
         return Serie::all();
+    }
+
+    public function store(Request $request)
+    {
+        return response()
+                ->json(
+                    Serie::create(['nome' => $request->nome]),
+                    201
+                );
+    }
+
+    public function get(int $id)
+    {
+        $serie = Serie::find($id);
+
+        if(is_null($serie)) {
+            return response()->json('', 204);
+        }
+
+        return response()->json($serie);
     }
 }

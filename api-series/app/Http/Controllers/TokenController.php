675074bea7097a946532d7545d3d4c1fb2266058
Letícia Fernandes <lemayara16@gmail.com>
2020-12-23 09:02:16 -0300

    comentários e mudança ao pegar as informações dos dados de autenticação devolvidos pelo JWT::decode

--- api-series/app/Http/Controllers/TokenController.php
+++ api-series/app/Http/Controllers/TokenController.php
@@ -11,24 +11,28 @@ class TokenController extends Controller
 {
     public function gerarToken(Request $request)
     {
-
+        // Validação dos campos
         $this->validate($request, [
             'email' => 'required|email',
             'password' => 'required',
         ]);
 
+        // Recuperar o Usuário
         $usuario = User::where('email', $request->email)->first();
 
+        // Verifica se há usuário e se a senha é válida
         if (is_null($usuario)
             || !Hash::check($request->password, $usuario->password)) {
             return response()->json('Usuário ou senha inválidos.', 401);
         }
 
+        // Cria o token
         $token = JWT::encode(
                                 ['email' => $request->email],
                                 env('JWT_KEY')
                             );
 
+        // Retorna um token
         return [
             'access_token' => $token
         ];
